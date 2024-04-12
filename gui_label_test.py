9e08e21eae99ef09abcc96a0133e31dd95d9daab
Yuan. <lin.siyuan@foxmail.com>
2024-04-12 17:55:59 +0800

    序列标签原型测试：多视图同步框选

--- gui_label_test.py
+++ gui_label_test.py
@@ -10,7 +10,8 @@ from PySide6.QtWidgets import (
     QPushButton,
     QDialog,
     QVBoxLayout,
-    QRadioButton
+    QRadioButton,
+    QWidget,
 )
 
 class LabelOption(QDialog):
@@ -53,22 +54,33 @@ class InteractivePlot(QMainWindow):
         self.setWindowTitle("Interactive Plot")
         self.resize(800, 600)
 
-        # 创建图形窗口
-        self.plot_widget = pg.PlotWidget(axisItems={'bottom': pg.DateAxisItem()})
-        self.setCentralWidget(self.plot_widget)
+        # 创建主窗口的布局
+        self.layout = QVBoxLayout()
+        central_widget = QWidget()
+        central_widget.setLayout(self.layout)
+        self.setCentralWidget(central_widget)
 
         # 读取CSV文件中的数据
         self.data = pd.read_csv("Umineko2018_small_data_LB07_lb0001.csv")
         self.data['datetime'] = pd.to_datetime(self.data['timestamp']).apply(lambda x: x.timestamp())
+
+        self.columnList = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'mag_x', 'mag_y', 'mag_z']
+        self.selectColumn = ['acc_x', 'acc_y', 'acc_z']
+        self.colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w']
+
+        # 绘制数据
+        self.plot_widgets = []
         self.plot_data()
 
         self.add_buttons()
         self.mode = 'add'
 
         self.click_begin = True
-        self.start_line = None
-        self.end_line = None
+        self.start_line = [None] * len(self.selectColumn)
+        self.end_line = [None] * len(self.selectColumn)
         self.regions = []
+        for _ in range(len(self.selectColumn)):
+            self.regions.append([])
 
     def add_buttons(self):
         self.add_mode = QPushButton("Add", self)
@@ -88,16 +100,17 @@ class InteractivePlot(QMainWindow):
         self.mode = mode
 
     def plot_data(self):
-        # 将数据拆分为时间戳和值
         timestamps = self.data['datetime']
-        values = self.data['acc_x']
+        for i, col in enumerate(self.selectColumn):
+            values = self.data[col]
+            pwidget = pg.PlotWidget(axisItems={'bottom': pg.DateAxisItem()})
+            pwidget.plot(timestamps, values, pen=self.colors[i])
 
-        # 绘制数据
-        self.plot_widget.plot(timestamps, values, pen='b')
-
-        # 添加交互功能
-        self.plot_widget.scene().sigMouseClicked.connect(self.mouse_clicked)
-        self.plot_widget.scene().sigMouseMoved.connect(self.mouse_moved)
+            # 添加交互功能
+            pwidget.scene().sigMouseClicked.connect(self.mouse_clicked)
+            pwidget.scene().sigMouseMoved.connect(self.mouse_moved)
+            self.layout.addWidget(pwidget)
+            self.plot_widgets.append(pwidget)
 
     def _to_idx(self, start_ts, end_ts):
         selected_indices = self.data[(self.data['datetime'] >= start_ts)
@@ -107,54 +120,70 @@ class InteractivePlot(QMainWindow):
     def _add_region(self, pos):
         if self.click_begin:    # 记录开始位置
             self.click_begin = False
-            self.start_line = pg.InfiniteLine(pos.x(), angle=90, movable=False)
-            self.end_line = pg.InfiniteLine(pos.x(), angle=90, movable=False)
-            self.plot_widget.addItem(self.start_line)
-            self.plot_widget.addItem(self.end_line)
+            for i, pwidget in enumerate(self.plot_widgets):
+                self.start_line[i] = pg.InfiniteLine(pos.x(), angle=90, movable=False)
+                self.end_line[i] = pg.InfiniteLine(pos.x(), angle=90, movable=False)
+                pwidget.addItem(self.start_line[i])
+                pwidget.addItem(self.end_line[i])
         else:                   # 记录结束位置
             self.click_begin = True
-            self.plot_widget.removeItem(self.start_line)
-            self.plot_widget.removeItem(self.end_line)
+            for i, pwidget in enumerate(self.plot_widgets):
+                pwidget.removeItem(self.start_line[i])
+                pwidget.removeItem(self.end_line[i])
 
-            region = pg.LinearRegionItem([self.start_line.value(), self.end_line.value()], brush=(0, 0, 255, 100))
+                region = pg.LinearRegionItem([self.start_line[i].value(), self.end_line[i].value()], brush=(0, 0, 255, 100))
+                region.sigRegionChanged.connect(self._region_changed)
 
-            self.start_line = None
-            self.end_line = None
+                self.start_line[i] = None
+                self.end_line[i] = None
 
-            self.plot_widget.addItem(region)
-            self.regions.append(region)
+                pwidget.addItem(region)
+                self.regions[i].append(region)
 
             start_idx, end_idx = self._to_idx(int(region.getRegion()[0]), int(region.getRegion()[1]))
             print(f'Selected range: from index {start_idx} to index {end_idx}')
 
-    def _del_region(self, pos):
-        for item in self.regions:
-            if item.getRegion()[0] < pos.x() and item.getRegion()[1] > pos.x():
-                self.plot_widget.removeItem(item)
-                self.regions.remove(item)
+    def _region_changed(self, region):
+        idx = 0
+        for reg_lst in self.regions:
+            for i, reg in enumerate(reg_lst):
+                if reg == region:
+                    idx = i
+                    break
+        for reg_lst in self.regions:
+            reg_lst[idx].setRegion(region.getRegion())
 
-                start_idx, end_idx = self._to_idx(int(item.getRegion()[0]), int(item.getRegion()[1]))
-                print(f'Delete region({start_idx}, {int(end_idx)})')
-                break
+    def _del_region(self, pos):
+        for i, pwidget in enumerate(self.plot_widgets):
+            for reg in self.regions[i]:
+                if reg.getRegion()[0] < pos.x() and reg.getRegion()[1] > pos.x():
+                    pwidget.removeItem(reg)
+                    self.regions[i].remove(reg)
+                    break
+        start_idx, end_idx = self._to_idx(int(reg.getRegion()[0]), int(reg.getRegion()[1]))
+        print(f'Delete region({start_idx}, {int(end_idx)})')
 
     def _edit_region(self, pos):
-        for item in self.regions:
-            if item.getRegion()[0] < pos.x() and item.getRegion()[1] > pos.x():
-                dialog = LabelOption()
-                if dialog.exec() == QDialog.Accepted:
-                    selected_option = dialog.confirm_selection()
-                    if selected_option == 'move':
-                        item.setBrush(QColor(255, 0, 0, 100))
-                    elif selected_option == 'stand':
-                        item.setBrush(QColor(0, 255, 0, 100))
-
-                    start_idx, end_idx = self._to_idx(int(item.getRegion()[0]), int(item.getRegion()[1]))
-                    print(f'Edit region({start_idx}, {end_idx}) label: {selected_option}')
-                    break
+        set_val = None
+        for i, _ in enumerate(self.regions):
+            for reg in self.regions[i]:
+                if reg.getRegion()[0] < pos.x() and reg.getRegion()[1] > pos.x():
+                    if set_val is None:
+                        dialog = LabelOption()
+                        if dialog.exec() == QDialog.Accepted:
+                            set_val = dialog.confirm_selection()
+                        else:
+                            set_val = 'None'
+                    if set_val == 'move':
+                        reg.setBrush(QColor(255, 0, 0, 100))
+                    elif set_val == 'stand':
+                        reg.setBrush(QColor(0, 255, 0, 100))
+        start_idx, end_idx = self._to_idx(int(reg.getRegion()[0]), int(reg.getRegion()[1]))
+        print(f'Edit region({start_idx}, {end_idx}) label: {set_val}')
 
     def mouse_clicked(self, event):
         if event.button() == Qt.LeftButton:
-            pos = self.plot_widget.plotItem.vb.mapSceneToView(event.pos()) # 将场景坐标映射到图形坐标系中
+            pos = self.plot_widgets[0].plotItem.vb.mapSceneToView(event.pos()) # 将场景坐标映射到图形坐标系中
 
             if self.mode == 'add':
                 self._add_region(pos)
@@ -164,9 +193,10 @@ class InteractivePlot(QMainWindow):
                 self._del_region(pos)
 
     def mouse_moved(self, event):
-        pos = self.plot_widget.plotItem.vb.mapSceneToView(event)
+        pos = self.plot_widgets[0].plotItem.vb.mapSceneToView(event)
         if not self.click_begin:
-            self.end_line.setPos(pos.x())
+            for line in self.end_line:
+                line.setPos(pos.x())
 
 
 if __name__ == "__main__":
