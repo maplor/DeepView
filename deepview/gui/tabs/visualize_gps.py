d15f94ccb68bfda17b58d404a962f8cf2efb0971
xiqxin <xiqxin@oulook.com>
2024-06-11 02:33:53 +0800

    GPS通过png显示

--- deepview/gui/tabs/visualize_gps.py
+++ deepview/gui/tabs/visualize_gps.py
@@ -1,4 +1,3 @@
-from PySide6.QtWebEngineWidgets import QWebEngineView
 
 from deepview.gui.components import (
     DefaultTab,
@@ -6,23 +5,23 @@ from deepview.gui.components import (
     _create_label_widget,
 )
 
-# from deepview.utils.auxiliaryfunctions import (
-#     read_config,
-#     get_unsupervised_set_folder
-# )
+
 from PySide6.QtWidgets import (
-    # QVBoxLayout,
+    QLabel,
     QPushButton,
     QWidget,
     QFileDialog,
     QScrollArea,
     QGridLayout
 )
+from PySide6.QtGui import QPixmap
 
-import folium
 import os
 import pickle
-# import pandas as pd
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
 
 class GPSDisplayer(DefaultTab):
     # # 定义进度信号
@@ -49,20 +48,7 @@ class GPSDisplayer(DefaultTab):
         self._generate_layout_attributes_dataset(self.dataset_attributes_dataset)
         self.main_layout.addLayout(self.dataset_attributes_dataset)
 
-        # #------------------------
-        # self.select_files_button = QPushButton("Select CSV Files")
-        # self.select_files_button.clicked.connect(self.select_files)
-        # self.main_layout.addWidget(self.select_files_button)
-        #
-        # self.scroll_area = QScrollArea()
-        # self.scroll_area_widget = QWidget()
-        # self.scroll_area_layout = QGridLayout()
-        # self.scroll_area_widget.setLayout(self.scroll_area_layout)
-        # self.scroll_area.setWidget(self.scroll_area_widget)
-        # self.scroll_area.setWidgetResizable(True)
-        # self.main_layout.addWidget(self.scroll_area)
-        # # self.main_layout.addWidget(self.selectfiles)
-        # self.main_layout.addLayout(self.dataset_attributes_dataset)
+
 
     def _generate_layout_attributes(self, layout):
         self.select_files_button = QPushButton("Select pkl Files")
@@ -79,8 +65,6 @@ class GPSDisplayer(DefaultTab):
         self.scroll_area.setWidget(self.scroll_area_widget)
         self.scroll_area.setWidgetResizable(True)
         layout.addWidget(self.scroll_area, 1, 0)
-        # self.main_layout.addWidget(self.selectfiles)
-        # self.main_layout.addLayout(self.dataset_attributes_dataset)
         return
 
     def select_files(self):
@@ -95,44 +79,62 @@ class GPSDisplayer(DefaultTab):
 
         row, col = 0, 0
         for file in self.selected_files:
-            try:
-                with open(file, 'rb') as f:
-                    tmpdf = pickle.load(f)
-                df = tmpdf[['datetime', 'latitude', 'longitude']].copy()
-                df = df.dropna()
-                latitudes = df['latitude']
-                longitudes = df['longitude']
-                timestamps = df['datetime']
-
-                # Validate the data
-                if latitudes.isnull().any() or longitudes.isnull().any() or timestamps.isnull().any():
-                    print(f"Data in {file} contains null values. Please check the pkl file.")
-                    continue
-
-                # Create a folium map centered around the average latitude and longitude
-                folium_map = folium.Map(location=[latitudes.mean(), longitudes.mean()], zoom_start=13)
-
-                # Add the GPS trajectory to the map
-                coordinates = list(zip(latitudes, longitudes))
-                folium.PolyLine(locations=coordinates, color='blue', weight=2.5).add_to(folium_map)
-
-                # Save the map to an HTML file
-                output_file = os.path.splitext(file)[0] + '_map.html'
-                folium_map.save(output_file)
-
-                # Load the HTML file into a QWebEngineView
-                web_view = QWebEngineView()
-                web_view.setFixedSize(400, 300)
-                web_view.setUrl(f'file:///{os.path.abspath(output_file)}')
-
-                # Add the QWebEngineView to the grid layout
-                self.scroll_area_layout.addWidget(web_view, row, col)
+            img_file = os.path.splitext(file)[0] + '_map.png'
+            if os.path.exists(img_file):
+                pixmap = QPixmap(img_file)
+                # Create a QLabel widget to display the image
+                label = QLabel()
+                label.setPixmap(pixmap)
+                self.scroll_area_layout.addWidget(label, row, col)
                 col += 1
                 if col >= 3:
                     col = 0
                     row += 1
+            else:
+                try:
+                    with open(file, 'rb') as f:
+                        tmpdf = pickle.load(f)
+                    df = tmpdf[['datetime', 'latitude', 'longitude']].copy()
+                    df = df.dropna()
+                    latitudes = df['latitude']
+                    longitudes = df['longitude']
+                    timestamps = df['datetime']
 
-                print(f"Trajectory map saved to {output_file}")
+                    gdf = gpd.GeoDataFrame(
+                        df, geometry=gpd.points_from_xy(df.longitude, df.latitude),
+                        crs="EPSG:4326"
+                    )
+                    # Create a plot
+                    fig, ax = plt.subplots()
+                    gdf.plot(ax=ax, marker='o', color='red', markersize=50)
 
-            except Exception as e:
-                print(f"An error occurred while processing {file}: {e}")
+                    # Set plot title and labels
+                    ax.set_title('GPS Coordinates on Map')
+                    ax.set_xlabel('Longitude')
+                    ax.set_ylabel('Latitude')
+
+                    plt.savefig(img_file)
+
+                    # Validate the data
+                    if latitudes.isnull().any() or longitudes.isnull().any() or timestamps.isnull().any():
+                        print(f"Data in {file} contains null values. Please check the pkl file.")
+                        continue
+
+                    # Add the QWebEngineView to the grid layout
+                    # Load the image
+                    pixmap = QPixmap(img_file)
+                    # Create a QLabel widget to display the image
+                    label = QLabel()
+                    label.setPixmap(pixmap)
+
+                    self.scroll_area_layout.addWidget(label, row, col)
+                    # self.scroll_area_layout.addWidget(web_view, row, col)
+                    col += 1
+                    if col >= 3:
+                        col = 0
+                        row += 1
+
+                    # print(f"Trajectory map saved to {output_file}")
+
+                except Exception as e:
+                    print(f"An error occurred while processing {file}: {e}")
