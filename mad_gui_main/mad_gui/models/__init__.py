507c4b6697246900c732498357f573081daa9c42
xiqxin <xiqxin@oulook.com>
2024-08-06 17:47:21 +0800

    remove mad_gui

--- mad_gui_main/mad_gui/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from mad_gui_main.mad_gui.models.global_data import GlobalData
-
-__all__ = ["GlobalData"]
--- mad_gui_main/mad_gui/plot_tools/plots/__init__.py
+++
@@ -1,9 +0,0 @@
-from mad_gui_main.mad_gui.plot_tools.plots.base_plot import BasePlot
-from mad_gui_main.mad_gui.plot_tools.plots.video_plot import VideoPlot
-from mad_gui_main.mad_gui.plot_tools.plots.sensor_plot import SensorPlot
-
-__all__ = [
-    "BasePlot",
-    "SensorPlot",
-    "VideoPlot",
-]
