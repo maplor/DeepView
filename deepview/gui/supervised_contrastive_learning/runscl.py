9eaaec6705b07bc8124bffd7c124d65ba643d1f7
xiqxin <xiqxin@oulook.com>
2024-08-06 17:47:21 +0800

    remove mad_gui

--- deepview/gui/supervised_contrastive_learning/runscl.py
+++ deepview/gui/supervised_contrastive_learning/runscl.py
@@ -256,15 +256,6 @@ def train(train_loader, model, criterion, optimizer, epoch, opt):
         batch_time.update(time.time() - end)
         end = time.time()
 
-        # # print info
-        # if (idx + 1) % opt.print_freq == 0:
-        #     print('Train: [{0}][{1}/{2}]\t'
-        #           'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-        #           'DT {data_time.val:.3f} ({data_time.avg:.3f})\t'
-        #           'loss {loss.val:.3f} ({loss.avg:.3f})'.format(
-        #         epoch, idx + 1, len(train_loader), batch_time=batch_time,
-        #         data_time=data_time, loss=losses))
-        #     sys.stdout.flush()
 
     if epoch == opt.epochs:
         # PCA latent representation to shape=(2, len) PCA降维到形状为 (2, len)
--- mad_gui_main/mad_gui/components/dialogs/__init__.py
+++
@@ -1,4 +0,0 @@
-from mad_gui_main.mad_gui.components.dialogs.user_information import UserInformation
-from mad_gui_main.mad_gui.components.dialogs.label_annotation_dialog import NestedLabelSelectDialog
-
-__all__ = ["UserInformation", "NestedLabelSelectDialog"]
