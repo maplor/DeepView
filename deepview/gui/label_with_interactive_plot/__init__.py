0297cfb7870f7eabb979688b9644f3f54567c87e
uangjunyan <77258338+uangjunyan@users.noreply.github.com>
2024-08-07 18:09:15 +0800

    更新代码
    1.鼠标单击时，坐标自动定位到最近点的判定，
    2.左边map上单击不同的点时控制动作都是平移
    3.让下拉菜单不卡

--- deepview/gui/label_with_interactive_plot/__init__.py
+++ deepview/gui/label_with_interactive_plot/__init__.py
@@ -810,17 +810,13 @@ class LabelWithInteractivePlot(QWidget):
         self.RawDatacomboBox = RawDatacomboBox
 
         # combbox change组合框改变时的处理
+        # 改变不再打开pkl，在点击dataplay再加载数据
         # 打开第一个.pkl文件
-        # with open(rawdata_file_path_list[0], 'rb') as f:
-        #     # 加载数据
-        #     self.data = pickle.load(f)
-        #     # 添加时间戳列
-        #     self.data['_timestamp'] = pd.to_datetime(self.data['datetime']).apply(lambda x: x.timestamp())
-        self.get_data_from_pkl(rawdata_file_path_list[0].name)
-        self.RawDatacomboBox.currentTextChanged.connect(
-            # 连接组合框文本改变事件到get_data_from_pkl方法
-            self.get_data_from_pkl
-        )
+        # self.get_data_from_pkl(rawdata_file_path_list[0].name)
+        # self.RawDatacomboBox.currentTextChanged.connect(
+        #     # 连接组合框文本改变事件到get_data_from_pkl方法
+        #     self.get_data_from_pkl
+        # )
         # 返回标签和组合框
         return RawDataComboBoxLabel, RawDatacomboBox
 
@@ -956,6 +952,8 @@ class LabelWithInteractivePlot(QWidget):
         self.isTarining = True
         # 更新按钮状态
         self.updateBtn()
+        # 获取combobox的内容
+        self.get_data_from_pkl(self.RawDatacomboBox.currentText())
 
         # 延时100毫秒调用handleComputeAsyn方法
         self.computeTimer.singleShot(100, self.handleComputeAsyn)
