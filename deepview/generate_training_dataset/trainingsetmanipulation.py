4b1f5634ed195a8d79b716d9f777a48df80168d3
maplor <maplor@163.com>
2026-05-31 22:07:55 +0800

    refactor: extract training dataset yaml helpers

--- deepview/generate_training_dataset/trainingsetmanipulation.py
+++ deepview/generate_training_dataset/trainingsetmanipulation.py
@@ -11,7 +11,6 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import yaml
 import glob
 import pickle
 from datetime import datetime
@@ -50,6 +49,11 @@ from deepview.generate_training_dataset.utils import (
 
 from deepview.utils.auxiliaryfunctions import (
     read_config,
+)
+from deepview.generate_training_dataset.yaml_config import (
+    MakeTest_pose_yaml,
+    MakeTrain_yaml,
+    ParseYaml,
 )
 
 
@@ -540,59 +544,3 @@ def create_training_dataset(
         "The training dataset is successfully created. Use the function 'train_network' to start training. Happy training!"
     )
     return
-
-# --------------make yaml files-------------------
-def ParseYaml(configfile):
-    raw = open(configfile).read()
-    docs = []
-    for raw_doc in raw.split("\n---"):
-        try:
-            docs.append(yaml.load(raw_doc, Loader=yaml.SafeLoader))
-        except SyntaxError:
-            docs.append(raw_doc)
-    return docs
-
-
-def MakeTrain_yaml(
-    itemstochange, saveasconfigfile, defaultconfigfile, items2drop={}
-):
-    docs = ParseYaml(defaultconfigfile)
-    for key in items2drop.keys():
-        # print(key, "dropping?")
-        if key in docs[0].keys():
-            docs[0].pop(key)
-
-    for key in itemstochange.keys():
-        docs[0][key] = itemstochange[key]
-
-    with open(saveasconfigfile, "w") as f:
-        yaml.dump(docs[0], f)
-
-    return docs[0]
-
-def MakeTest_pose_yaml(
-    dictionary,
-    keys2save,
-    saveasfile,
-    nmsradius=None,
-    minconfidence=None,
-    sigma=None,
-    locref_smooth=None,
-):
-    dict_test = {}
-    for key in keys2save:
-        dict_test[key] = dictionary[key]
-
-    # adding important values for multianiaml project:
-    if nmsradius is not None:
-        dict_test["nmsradius"] = nmsradius
-    if minconfidence is not None:
-        dict_test["minconfidence"] = minconfidence
-    if sigma is not None:
-        dict_test["sigma"] = sigma
-    if locref_smooth is not None:
-        dict_test["locref_smooth"] = locref_smooth
-
-    dict_test["scoremap_dir"] = "test"
-    with open(saveasfile, "w") as f:
-        yaml.dump(dict_test, f)
